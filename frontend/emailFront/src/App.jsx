7984537a28e8ec978f1ecb1d000fc0e00536252e
sidikh-smi <80346822+sidikh-smi@users.noreply.github.com>
2026-03-11 17:47:18 +0000

    I think it's good

--- frontend/emailFront/src/App.jsx
+++ frontend/emailFront/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login/Login";
 import Registre from "./pages/Registre/Registre";
@@ -8,12 +8,19 @@ import GenerateAi from "./pages/GenerateAi/GenerateAi";
 import DashboardLayout from "./DashboardLayout";
 import Clients from "./pages/Clients/Clients";
 
+const RedirectHandler = () => {
+  const token = localStorage.getItem("token");
+  console.log("Checking token for redirect:", token);
+  return token ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+};
+
 const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/registre" element={<Registre />} />
 
+      <Route path="*" element={<RedirectHandler />} />
       <Route element={<DashboardLayout />}>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="clients" element={<Clients />} />
